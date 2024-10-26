b004a870a0618c868015419e023cb5977175ac0e
Kabir Deula <kabirdeula167@gmail.com>
2024-10-26 16:03:20 +0545

    feat: ✨Implement Rule model and use Rules from assets

---
+++ lib/data/models/rule_model.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'rule_model.freezed.dart';
+part 'rule_model.g.dart';
+
+@freezed
+class RuleModel with _$RuleModel {
+  const factory RuleModel({
+    required String title,
+    required String description,
+  }) = _RuleModel;
+
+  factory RuleModel.fromJson(Map<String, dynamic> json) =>
+      _$RuleModelFromJson(json);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:wild_dare_randomizer/app/app.dart';
+import 'package:wild_dare_randomizer/data/models/model.dart';
 
 import 'screen.dart';
 
@@ -12,29 +16,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Map<String, String> predefinedRules = {
-    'Skip Two Turns': 'The next player skips two turns.',
-    'Discard Same Color':
-        'You can discard all cards of the same color in one turn.',
-    'Take 15 Cards': 'You must draw 15 cards from the deck.',
-    'Reverse Skip':
-        'The turn order is reversed, and the next player is skipped.',
-    'Stack Draws': 'You can stack draw cards until the next player draws.',
-    'All Players Draw 5': 'All players must draw 5 cards.',
-    'Discard Same Number':
-        'You can discard all cards with the same number at once.',
-    'Swap Hands': 'You can swap hands with any player of your choice.',
-    'Free Pass': 'You get a free pass and skip your turn.',
-    'Wild Draw Skip': 'You can play a wild draw card and skip the next player.',
-    'Discard or Draw': 'You either discard one card or draw one card.',
-    'Silent Round': 'No talking is allowed for the rest of the round.',
-    'Shuffle but Escape': 'Shuffle the deck but skip your turn.',
-    'Trade Hands': 'Trade hands with the player next to you.',
-    'No Special Cards': 'Special cards cannot be played until the next round.',
-    'Ultimate Uno': 'All players must play with open cards for one round.'
-  };
-
-  List<MapEntry<String, String>> randomizedRules = [];
+  List<RuleModel> randomizedRules = [];
   bool isGridView = false;
 
   @override
@@ -44,40 +26,50 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void loadInitialRules() async {
+    List<RuleModel> predefinedRules = await loadRulesFromJson();
+
     var box = Hive.box(Config.kHiveBox);
-    List<MapEntry<String, String>> customRules = [];
+    List<RuleModel> customRules = [];
 
     for (var rule in box.values) {
       if (rule is Map &&
           rule.containsKey('title') &&
           rule.containsKey('description')) {
-        customRules.add(MapEntry(rule['title'], rule['description']));
+        customRules.add(
+          RuleModel(title: rule['title'], description: rule['description']),
+        );
       }
     }
 
     setState(() {
-      randomizedRules = [...predefinedRules.entries, ...customRules];
+      randomizedRules = [...predefinedRules, ...customRules];
       randomizedRules.shuffle();
       randomizedRules = randomizedRules.take(16).toList();
     });
   }
 
+  Future<List<RuleModel>> loadRulesFromJson() async {
+    final String response = await rootBundle.loadString('assets/rules.json');
+    final List<dynamic> data = json.decode(response);
+
+    return data.map((json) => RuleModel.fromJson(json)).toList();
+  }
+
   void randomizeRules() async {
     var box = Hive.box(Config.kHiveBox);
-    List<MapEntry<String, String>> customRules = [];
+    List<RuleModel> customRules = [];
 
     for (var rule in box.values) {
       if (rule is Map &&
           rule.containsKey('title') &&
           rule.containsKey('description')) {
-        customRules.add(MapEntry(rule['title'], rule['description']));
+        customRules.add(
+          RuleModel(title: rule['title'], description: rule['description']),
+        );
       }
     }
 
-    List<MapEntry<String, String>> allRules = [
-      ...predefinedRules.entries,
-      ...customRules
-    ];
+    List<RuleModel> allRules = [...await loadRulesFromJson(), ...customRules];
     allRules.shuffle();
 
     setState(() {
@@ -134,8 +126,8 @@ class _HomeScreenState extends State<HomeScreen> {
       itemCount: randomizedRules.length,
       itemBuilder: (context, index) {
         return ListTile(
-          title: Text('${index + 1}: ${randomizedRules[index].key}'),
-          subtitle: Text(randomizedRules[index].value),
+          title: Text('${index + 1}: ${randomizedRules[index].title}'),
+          subtitle: Text(randomizedRules[index].description),
         );
       },
     );
@@ -145,7 +137,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 1.5,
+        childAspectRatio: 1,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
@@ -158,13 +150,13 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  '${index + 1}: ${randomizedRules[index].key}',
+                  '${index + 1}: ${randomizedRules[index].title}',
                   style: const TextStyle(fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 8),
                 Text(
-                  randomizedRules[index].value,
+                  randomizedRules[index].description,
                   textAlign: TextAlign.center,
                 ),
               ],
