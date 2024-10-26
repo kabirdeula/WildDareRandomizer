b7587beade271ef1ebb362c751e363980fb013aa
Kabir Deula <kabirdeula167@gmail.com>
2024-10-26 22:30:16 +0545

    fix: 🐞Implement hive_services

--- lib/data/repositories/rule_repository.dart
+++ lib/data/repositories/rule_repository.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:wild_dare_randomizer/app/app.dart';
 import 'package:wild_dare_randomizer/data/models/model.dart';
+import 'package:wild_dare_randomizer/data/sources/hive_services.dart';
 
 class RuleRepository {
+  final HiveServices _hiveService = HiveServices();
   Future<List<RuleModel>> loadRulesFromJson() async {
     final String response = await rootBundle.loadString('assets/rules.json');
     final List<dynamic> data = json.decode(response);
@@ -13,25 +13,9 @@ class RuleRepository {
     return data.map((json) => RuleModel.fromJson(json)).toList();
   }
 
-  Future<List<RuleModel>> loadCustomRulesFromHive() async {
-    var box = Hive.box(Config.kHiveBox);
-    List<RuleModel> customRules = [];
-
-    for (var rule in box.values) {
-      if (rule is Map &&
-          rule.containsKey('title') &&
-          rule.containsKey('description')) {
-        customRules.add(
-          RuleModel(title: rule['title'], description: rule['description']),
-        );
-      }
-    }
-    return customRules;
-  }
-
   Future<List<RuleModel>> fetchAllRules() async {
     List<RuleModel> predefinedRules = await loadRulesFromJson();
-    List<RuleModel> customRules = await loadCustomRulesFromHive();
+    List<RuleModel> customRules = await _hiveService.fetchAllRules();
 
     List<RuleModel> allRules = [...predefinedRules, ...customRules];
     allRules.shuffle();
