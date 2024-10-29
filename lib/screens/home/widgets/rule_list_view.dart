2ea6c4ebe02a8f16a494714734b36bc3b6022d6c
Kabir Deula <kabirdeula167@gmail.com>
2024-10-29 16:13:01 +0545

    fix: 🐞Add Colors to ListTile

--- lib/screens/home/widgets/rule_list_view.dart
+++ lib/screens/home/widgets/rule_list_view.dart
@@ -11,11 +11,28 @@ class RuleListView extends StatelessWidget {
     return ListView.builder(
       itemCount: rules.length.clamp(0, 16),
       itemBuilder: (context, index) {
+        final tileColor = _getTileColor(index);
         return ListTile(
+          tileColor: tileColor,
           title: Text('${index + 1}: ${rules[index].title}'),
           subtitle: Text(rules[index].description),
         );
       },
     );
+  }
+
+  Color _getTileColor(int index) {
+    switch (index % 4) {
+      case 0:
+        return Colors.red;
+      case 1:
+        return Colors.blue;
+      case 2:
+        return Colors.green;
+      case 3:
+        return Colors.yellow;
+      default:
+        return Colors.purple;
+    }
   }
 }
