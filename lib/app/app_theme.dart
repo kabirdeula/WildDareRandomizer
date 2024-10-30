f509f3117a1755ecf32a0b7674abc0895c403782
Kabir Deula <kabirdeula167@gmail.com>
2024-10-30 11:54:21 +0545

    feat: ✨Add ThemeService

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -3,34 +3,34 @@ import 'package:wild_dare_randomizer/app/app.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
-    primaryColor: AppColor.kLightPrimaryColor,
-    colorScheme: ColorScheme.light(
-      primary: AppColor.kLightPrimaryColor,
-      secondary: AppColor.kLightSecondaryColor,
-      surface: AppColor.kBgPrimaryColor,
-      onSurface: AppColor.kBgSecondaryColor,
-    ),
-    scaffoldBackgroundColor: AppColor.kBackgroundColor,
-    appBarTheme: AppBarTheme(
-      backgroundColor: AppColor.kBackgroundColor,
-      foregroundColor: AppColor.kLightPrimaryColor,
-    ),
+    // primaryColor: AppColor.kLightPrimaryColor,
+    // colorScheme: ColorScheme.light(
+    //   primary: AppColor.kLightPrimaryColor,
+    //   secondary: AppColor.kLightSecondaryColor,
+    //   surface: AppColor.kBgPrimaryColor,
+    //   onSurface: AppColor.kBgSecondaryColor,
+    // ),
+    // scaffoldBackgroundColor: AppColor.kBackgroundColor,
+    // appBarTheme: AppBarTheme(
+    //   backgroundColor: AppColor.kBackgroundColor,
+    //   foregroundColor: AppColor.kLightPrimaryColor,
+    // ),
     brightness: Brightness.light,
   );
 
   static final ThemeData darkTheme = ThemeData(
-    primaryColor: AppColor.kDarkModePrimaryColor,
-    colorScheme: ColorScheme.dark(
-      primary: AppColor.kDarkModePrimaryColor,
-      secondary: AppColor.kDarkModeSecondaryColor,
-      surface: AppColor.kBgPrimaryColor,
-      onSurface: Colors.white,
-    ),
-    scaffoldBackgroundColor: AppColor.kBgPrimaryColor,
-    appBarTheme: AppBarTheme(
-      backgroundColor: AppColor.kBackgroundColor,
-      foregroundColor: AppColor.kDarkModePrimaryColor,
-    ),
+    // primaryColor: AppColor.kDarkModePrimaryColor,
+    // colorScheme: ColorScheme.dark(
+    //   primary: AppColor.kDarkModePrimaryColor,
+    //   secondary: AppColor.kDarkModeSecondaryColor,
+    //   surface: AppColor.kBgPrimaryColor,
+    //   onSurface: Colors.white,
+    // ),
+    // scaffoldBackgroundColor: AppColor.kBgPrimaryColor,
+    // appBarTheme: AppBarTheme(
+    //   backgroundColor: AppColor.kBackgroundColor,
+    //   foregroundColor: AppColor.kDarkModePrimaryColor,
+    // ),
     brightness: Brightness.dark,
   );
 }
