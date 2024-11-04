b38797439b7053f492cae8b4598106ff6c974d0a
Kabir Deula <kabirdeula167@gmail.com>
2024-11-04 14:48:18 +0545

    feat: ✨Add settings_model

---
+++ lib/data/models/settings_model.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'settings_model.freezed.dart';
+part 'settings_model.g.dart';
+
+@freezed
+class SettingsModel with _$SettingsModel {
+  const factory SettingsModel({
+    required bool isDarkMode,
+    required bool isGridView,
+  }) = _SettingsModel;
+
+  factory SettingsModel.fromJson(Map<String, dynamic> json) =>
+      _$SettingsModelFromJson(json);
+}
---
+++ lib/data/models/settings_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SettingsModelImpl _$$SettingsModelImplFromJson(Map<String, dynamic> json) =>
+    _$SettingsModelImpl(
+      isDarkMode: json['isDarkMode'] as bool,
+      isGridView: json['isGridView'] as bool,
+    );
+
+Map<String, dynamic> _$$SettingsModelImplToJson(_$SettingsModelImpl instance) =>
+    <String, dynamic>{
+      'isDarkMode': instance.isDarkMode,
+      'isGridView': instance.isGridView,
+    };
