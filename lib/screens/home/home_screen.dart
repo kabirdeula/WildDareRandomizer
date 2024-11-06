3ab6e71e7e1e49fc27fbda6668a46060e3e6b884
Kabir Deula <kabirdeula167@gmail.com>
2024-11-06 12:18:52 +0545

    feat: ✨Add PageView

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,57 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:wild_dare_randomizer/providers/provider.dart';
-import 'package:wild_dare_randomizer/screens/home/widgets/home_widget.dart';
-import 'package:wild_dare_randomizer/utils/enums/routes.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final rulesAsyncValue = ref.watch(rulesProvider);
-    final settings = ref.watch(settingsProvider);
-    final bool isGridView = settings.isGridView;
-
+    final selectedIndex = ref.watch(selectedIndexProvider);
+    final pageController = ref.watch(pageControllerProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Uno Dare Randomizer'),
-        actions: [
-          IconButton(
-            onPressed: () => context.push(Routes.rules.path),
-            icon: const Icon(Icons.rule),
+        centerTitle: true,
+        title: Text(
+          'Uno Dare Randomizer',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (index) =>
+            ref.read(selectedIndexProvider.notifier).state = index,
+        children: const [],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: selectedIndex,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
           ),
-          const ViewSettingsMenu(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(ruleRepositoryProvider).shuffleRules().then(
-                (_) => ref.refresh(rulesProvider),
-              );
-        },
-        child: const Icon(Icons.shuffle),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: rulesAsyncValue.when(
-              data: (rules) {
-                return isGridView
-                    ? RuleGridView(rules: rules)
-                    : RuleListView(rules: rules);
-              },
-              error: (error, stack) => Center(
-                child: Text('Error : $error'),
-              ),
-              loading: () => const Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
+          NavigationDestination(
+            icon: Icon(Icons.favorite_outline),
+            selectedIcon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings),
+            label: 'Settings',
           ),
         ],
       ),
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:wild_dare_randomizer/providers/provider.dart';
+// import 'package:wild_dare_randomizer/screens/home/widgets/home_widget.dart';
+// import 'package:wild_dare_randomizer/utils/enums/routes.dart';
+
+// class HomeScreen extends ConsumerWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final rulesAsyncValue = ref.watch(rulesProvider);
+//     final settings = ref.watch(settingsProvider);
+//     final bool isGridView = settings.isGridView;
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text(
+//           'Uno Dare Randomizer',
+//           style: Theme.of(context).textTheme.titleLarge,
+//         ),
+//         actions: [
+//           IconButton(
+//             onPressed: () => context.push(Routes.rules.path),
+//             icon: const Icon(Icons.rule),
+//           ),
+//           const ViewSettingsMenu(),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           ref.read(ruleRepositoryProvider).shuffleRules().then(
+//                 (_) => ref.refresh(rulesProvider),
+//               );
+//         },
+//         child: const Icon(Icons.shuffle),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: rulesAsyncValue.when(
+//               data: (rules) {
+//                 return isGridView
+//                     ? RuleGridView(rules: rules)
+//                     : RuleListView(rules: rules);
+//               },
+//               error: (error, stack) => Center(
+//                 child: Text('Error : $error'),
+//               ),
+//               loading: () => const Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
