a00f26498fc9eacb519c31ac6fd5f04acea4228e
Kabir Deula <kabirdeula167@gmail.com>
2024-11-10 12:52:57 +0545

    docs: 📃Update Config Docs

--- lib/app/config.dart
+++ lib/app/config.dart
@@ -1,7 +1,13 @@
 class Config {
+  /// App Name - `Uno Dare Randomizer`
   static const String kAppName = 'Uno Dare Randomizer';
+
+  /// App Version - `1.0.0`
   static const String kAppVersion = '1.0.0';
 
+  /// Rule Box = `rules`
   static const String kRuleBox = 'rules';
+
+  /// Settings Box = `settings`
   static const String kSettingsBox = 'settings';
 }
