435c966baee08e9e32887b93f3ed514b27353735
Kabir Deula <kabirdeula167@gmail.com>
2024-11-10 13:27:53 +0545

    docs: 📃Add docs for log_service

--- lib/data/sources/log_service.dart
+++ lib/data/sources/log_service.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
+/// A service for logging messages with different log levels.
+///
+/// `LogService` utilizes the `Logger` package to handle logging throughout
+/// the app, adjusting its log level depending on the build mode.
+///
+/// - In `debug` mode, the log level is set to `Level.debug`, allowing
+/// detailed log messages to be captured.
+/// - In `release` mode, the log level is set to `Level.error`, limiting
+/// logs to critical error messages for production efficiency.
 class LogService {
+  /// The instance of `Logger` used for logging in the app.
+  ///
+  /// Configured to only log error messages in release mode and debug
+  /// messages in debug mode, reducing unnecessary logs in production.
   static final Logger logger = Logger(
     level: kReleaseMode ? Level.error : Level.debug,
   );
