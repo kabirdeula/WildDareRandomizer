c7b270e617ce28d4885c71dea10a7fa7b14a4a4e
Kabir Deula <kabirdeula167@gmail.com>
2024-11-13 18:50:41 +0545

    docs: 📃Add documentation for theme_service

--- lib/data/sources/theme_service.dart
+++ lib/data/sources/theme_service.dart
@@ -3,14 +3,27 @@ import 'package:wild_dare_randomizer/app/app.dart';
 import 'package:wild_dare_randomizer/data/models/settings_model.dart';
 import 'package:wild_dare_randomizer/utils/util.dart';
 
+/// A service class that manages theme-related settings and preferences for the app.
+/// This service provides methods to load, toggle, and save theme settings.
+/// by interacting with a local Hive database.
 class ThemeService {
+  /// The Hive box containing theme settings.
   late final Future<Box> _box;
+
+  /// The key used to store the theme settings in the Hive box.
   static const _settingsKey = 'settings';
 
+  /// Initializes a new instance of [ThemeService].
+  /// This constructor opens the Hive box for theme settings.
   ThemeService() {
     _box = HiveUtil.openHiveBox(Config.kSettingsBox);
   }
 
+  /// Loads the theme settings from the Hive box.
+  ///
+  /// Returns a [SettingsModel] representing the current theme settings.
+  /// If no settings are saved, it returns a default [SettingsModel] with
+  /// default values.
   Future<SettingsModel> loadSettings() async {
     try {
       final box = await _box;
@@ -24,6 +37,12 @@ class ThemeService {
     }
   }
 
+  /// Toggles the theme mode between light and dark based on the given [isDarkMode] value.
+  ///
+  /// This method updates the stored settings with the new theme preference
+  /// and saves it to the Hive box.
+  ///
+  /// * [isDarkMode]: A boolean value indicating whether dark mode is enabled.
   Future<void> toggleTheme(bool isDarkMode) async {
     try {
       final settings = await loadSettings();
@@ -35,6 +54,10 @@ class ThemeService {
     }
   }
 
+  /// Loads the current theme preference, returning `true` if dark mode is enabled.
+  ///
+  /// Returns a [bool] indicating the current theme preference.
+  /// This method fetches the value of `isDarkMode` from the [SettingsModel].
   Future<bool> loadTheme() async {
     final settings = await loadSettings();
     return settings.isDarkMode;
