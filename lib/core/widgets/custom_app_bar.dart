e1ce96a941081700fffb200333d612dc964532b2
Kabir Deula <kabirdeula167@gmail.com>
2024-12-19 19:30:47 +0545

    feat: ✨add CustomAppBar widget for consistent app bar styling

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:wild_dare_randomizer/core/constants/constants.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String? title;
+
+  final List<Widget>? actions;
+  final Widget? leading;
+  final bool showBackButton;
+
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.actions,
+    this.leading,
+    this.showBackButton = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: showBackButton ? BackButton(color: AppColors.text) : leading,
+      title: Text(title ?? ''),
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
