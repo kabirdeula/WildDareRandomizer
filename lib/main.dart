a0f77ee83c9cd74301f0ccaf18917412bd9374f3
Kabir Deula <kabirdeula167@gmail.com>
2024-12-19 19:20:43 +0545

    feat: ✨refactor main entry point to use Bloc and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,69 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:wild_dare_randomizer/utils/router/app_routes.dart';
 
-import 'app/app.dart';
-import 'data/repositories/repository.dart';
-import 'providers/provider.dart';
-import 'utils/util.dart';
+import 'core/themes/themes.dart';
+import 'core/utils/utils.dart';
+import 'dependency_injection.dart';
+import 'features/gateway/gateway.dart';
+import 'routes/routes.dart';
 
+/// Entry point of the Flutter application.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Hive.initFlutter();
-    await Hive.openBox(Config.kRuleBox);
-    final ruleRepository = RuleRepository();
-    await ruleRepository.loadRulesIfNeeded();
-  } catch (e) {
-    log.e("Error initializing App Dependencies: $e");
-  }
-  runApp(
-    DevicePreview(
+
+  await Hive.initFlutter();
+
+  dependencyInjection();
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (context) => locator<GatewayCubit>()),
+    ],
+    child: DevicePreview(
       enabled: !kReleaseMode,
-      builder: (context) => const ProviderScope(child: MyApp()),
+      builder: (context) => const MyApp(),
     ),
-  );
+  ));
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final settings = ref.watch(settingsProvider);
+  Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360, 640),
       minTextAdapt: true,
-      builder: (context,child) {
+      splitScreenMode: false,
+      builder: (context, child) {
         return MaterialApp.router(
+          builder: (context, child) {
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: child!,
+            );
+          },
           locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          debugShowCheckedModeBanner: false,
-          routerConfig: AppRoutes.router,
-          title: Config.kAppName,
-          theme: settings.isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: !kReleaseMode ? true : false,
+          theme: AppThemes.themeData(),
         );
-      }
+      },
     );
   }
 }
+
+// void main() async {
+//   try {
+//     await Hive.openBox(Config.kRuleBox);
+//     final ruleRepository = RuleRepository();
+//     await ruleRepository.loadRulesIfNeeded();
+//   } catch (e) {
+//     log.e("Error initializing App Dependencies: $e");
+//   }
+// }
