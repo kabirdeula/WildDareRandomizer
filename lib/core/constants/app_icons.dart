17d598e9264b0e139881c5d74777f51b6c61ebc3
Kabir Deula <kabirdeula167@gmail.com>
2024-12-20 16:06:07 +0545

    feat: ✨add AppIcons class for centralized icon management and update exports

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const IconData home = Icons.home;
+  static const IconData homeOff = Icons.home_outlined;
+
+  static const IconData rules = Icons.rule;
+  static const IconData rulesOff = Icons.rule_outlined;
+
+  static const IconData settings = Icons.settings;
+  static const IconData settingsOff = Icons.settings_outlined;
+}
