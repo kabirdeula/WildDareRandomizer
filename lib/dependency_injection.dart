527a3285ddb74a63367694dfa875846cdf3a4b4d
Kabir Deula <kabirdeula167@gmail.com>
2024-12-20 16:06:45 +0545

    feat: ✨add DashboardCubit registration to dependency injection

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,9 +1,14 @@
 import 'core/utils/utils.dart';
+import 'features/dashboard/dashboard.dart';
 import 'features/gateway/gateway.dart';
 
 void dependencyInjection() {
   // * Cubit
   locator.registerFactory<GatewayCubit>(
     () => GatewayCubit(),
+  );
+
+  locator.registerFactory<DashboardCubit>(
+    () => DashboardCubit(),
   );
 }
