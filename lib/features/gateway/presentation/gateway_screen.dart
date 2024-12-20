bc127b18a788e8a6d6ce39f3223534c80789d4be
Kabir Deula <kabirdeula167@gmail.com>
2024-12-20 16:05:44 +0545

    feat: ✨refactor GatewayScreen to use StatelessWidget and improve app loading logic

--- lib/features/gateway/presentation/gateway_screen.dart
+++ lib/features/gateway/presentation/gateway_screen.dart
@@ -4,22 +4,53 @@ import 'package:wild_dare_randomizer/features/gateway/cubit/gateway_cubit.dart';
 import 'package:wild_dare_randomizer/routes/routes.dart';
 import 'package:wild_dare_randomizer/utils/util.dart';
 
-class GatewayScreen extends StatefulWidget {
+// class GatewayScreen extends StatefulWidget {
+//   const GatewayScreen({super.key});
+
+//   @override
+//   State<GatewayScreen> createState() => _GatewayScreenState();
+// }
+
+// class _GatewayScreenState extends State<GatewayScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     context.read<GatewayCubit>().appLoaded();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocConsumer<GatewayCubit, GatewayState>(
+//       builder: (context, state) {
+//         return Scaffold(
+//           body: Center(
+//             child: state.isLoading
+//                 ? const CircularProgressIndicator()
+//                 : Text('Gateway Screen\n$state'),
+//           ),
+//         );
+//       },
+//       listener: (context, state) {
+//         log.i('(GatewayScreen) listener: $state');
+//         if (state.isLoading == false) {
+//           context.go(AppRoutes.dashboard.path);
+//         } else {
+//           return;
+//         }
+//       },
+//     );
+//   }
+// }
+
+class GatewayScreen extends StatelessWidget {
   const GatewayScreen({super.key});
 
   @override
-  State<GatewayScreen> createState() => _GatewayScreenState();
-}
-
-class _GatewayScreenState extends State<GatewayScreen> {
-  @override
-  void initState() {
-    super.initState();
-    context.read<GatewayCubit>().appLoaded();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Future.microtask(() {
+      context.read<GatewayCubit>().appLoaded();
+    });
+
     return BlocConsumer<GatewayCubit, GatewayState>(
       builder: (context, state) {
         return Scaffold(
