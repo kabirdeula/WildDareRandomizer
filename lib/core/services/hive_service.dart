36c424dcad9d8acaa62bdfb65bf4dfeac22b16e8
Kabir Deula <kabirdeula167@gmail.com>
2026-01-17 22:33:49 +0545

    feat: add HiveService and LogService

---
+++ lib/core/services/hive_service.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive_ce_flutter/hive_ce_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../../features/rules/rules.dart';
+import '../core.dart';
+
+abstract final class HiveService {
+  static final List<String> fileName = ["HiveService"];
+  static Future<void> init() async {
+    if (kIsWeb) {
+      await Hive.initFlutter();
+    } else {
+      final appDocumentDir = await getApplicationDocumentsDirectory();
+      await Hive.initFlutter(appDocumentDir.path);
+    }
+
+    // * Register Adapters
+    Hive.registerAdapter(RuleEntityAdapter());
+
+    log.d("Initialized Hive and registered adapters.", tags: fileName);
+  }
+
+  static Future<void> clearAllData() async {
+    await clearBox<RuleEntity>(boxName: ruleEntityKey.name);
+  }
+
+  static Future<Box<T>> openBox<T>({required String boxName}) async {
+    try {
+      if (!Hive.isBoxOpen(boxName)) {
+        log.d("Opening box: $boxName", tags: fileName);
+        return await Hive.openBox<T>(boxName);
+      }
+
+      return Hive.box<T>(boxName);
+    } catch (e) {
+      log.e("Failed to open box: $boxName\n$e", tags: fileName);
+      throw HiveError(e.toString());
+    }
+  }
+
+  static Future<void> clearBox<T>({required String boxName}) async {
+    final box = await openBox<T>(boxName: boxName);
+    await box.clear();
+  }
+
+  static Future<void> closeAll() async => await Hive.close();
+
+  // static Future<void> registerBox<T>({required HiveBoxKey<T> boxKey}) async {
+  //   final box = await openBox<T>(boxName: boxKey.name);
+  //   log.d("Registered Hive Box: ${boxKey.name}", tags: fileName);
+  // }
+}
+
+class HiveBoxKey<T> {
+  final String name;
+
+  const HiveBoxKey({required this.name});
+}
+
+abstract final class HiveBoxes {
+  static const String ruleEntity = 'RuleEntity';
+}
+
+const ruleEntityKey = HiveBoxKey<RuleEntity>(name: HiveBoxes.ruleEntity);
