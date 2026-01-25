5f17af6a85ba61cbcb4849e70e54cd217a99ffe2
Kabir Deula <kabirdeula167@gmail.com>
2026-01-25 09:46:36 +0545

    feat: add extension to darken and lighten colors

--- lib/core/extensions/extension.dart
+++ lib/core/extensions/extension.dart
@@ -1,8 +1,28 @@
-import 'dart:ui';
+import 'package:flutter/widgets.dart';
 
 extension ColorOpacityX on Color {
   Color withOpacitySafe(double opacity) {
     assert(opacity >= 0 && opacity <= 1);
     return withAlpha((opacity * 255).round());
+  }
+}
+
+extension ColorBrightness on Color {
+  /// Darken a color by a percentage (0.0 to 1.0)
+  Color darken([double amount = 0.1]) {
+    assert(amount >= 0 && amount <= 1);
+    final hsl = HSLColor.fromColor(this);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+    return hslDark.toColor();
+  }
+
+  /// Lighten a color by a percentage (0.0 to 1.0)
+  Color lighten([double amount = 0.1]) {
+    assert(amount >= 0 && amount <= 1);
+    final hsl = HSLColor.fromColor(this);
+    final hslLight = hsl.withLightness(
+      (hsl.lightness + amount).clamp(0.0, 1.0),
+    );
+    return hslLight.toColor();
   }
 }
