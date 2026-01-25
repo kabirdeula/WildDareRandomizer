6ed1d198a919a280511e772d554675d0d33a3adf
Kabir Deula <kabirdeula167@gmail.com>
2026-01-25 09:45:54 +0545

    feat: add global shimmer

---
+++ lib/common/widgets/app_shimmer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class AppShimmer extends StatelessWidget {
+  final Widget child;
+  final Color? baseColor;
+  final Color? highlightColor;
+
+  const AppShimmer({
+    super.key,
+    required this.child,
+    this.baseColor,
+    this.highlightColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: baseColor ?? Colors.grey.shade300,
+      highlightColor: highlightColor ?? Colors.grey.shade100,
+      child: child,
+    );
+  }
+}
